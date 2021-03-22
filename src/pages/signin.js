2d7f630af8608e9ee61fd2530dd1d3a56789baeb
Koras02 <wjdgh0727@gmail.com>
2021-03-22 21:28:18 +0900

    중간저장 3:45:49

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin() {
-  return <p>Hello from the SignIn</p>;
+  const [emailAdress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState("");
+
+  // 아무 것도 입력 안할시?? 버튼 비활성화
+  const isInvalid = password === "" || emailAdress === "";
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    // firbase work here!!
+  };
+
+  // check form input elements are valid
+  // email & password
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="이메일 주소"
+              value={emailAdress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="비밀번호"
+              autoComplate="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
