93d986e071d618dc3fd743924271124b86a4a7ea
Koras02 <wjdgh0727@gmail.com>
2021-03-22 13:25:17 +0900

    메인 Component 구성

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import React from "react";
+import jumboData from "./fixtures/jumbo"; // jumboData불러오기
 import Jumbotron from "./components/jumbotron/index";
 
 export default function App() {
-  return <Jumbotron />;
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          {/* item 목록 불러오기 */}
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
--- src/components/jumbotron/style/jumbotron.js
+++ src/components/jumbotron/style/jumbotron.js
@@ -1,6 +1,60 @@
 // 사용자 동작을 기록해서 재사용해주는 방법으로 반복되는 작업이 줄어든다
 import styled from "styled-components/macro";
 
-export const Inner = styled.div`
+export const Item = styled.div`
   display: flex;
+  border-bottom: 8px solid #222;
+  padding: 50px 5%;
+  color: white;
+  overflow: hidden;
 `;
+
+export const Inner = styled.section`
+  display: flex;
+  align-items: center;
+    flex-direction: ${({ direction }) => direction}
+  justify-content: space-between;
+  max-width: 1100px;
+  margin: auto;
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    flex-direction: column; // 1000px 일때 가운데로
+  }
+`;
+
+export const Pane = styled.div`
+  width: 50%;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    padding: 0 5px;
+    text-align: center;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
+
+  @media screen and (max-width: 600px) {
+    font-size: 20px;
+  } ;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+
+  @media screen and (max-width: 600px) {
+    font-size: 20px;
+  }
+`;
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
+export const Container = styled.div``;
