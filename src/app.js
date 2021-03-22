e4996607ed4b2b191600e9964b8549829db62898
Koras02 <wjdgh0727@gmail.com>
2021-03-22 23:45:30 +0900

    firebase로 브라우저 목록불러오기

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, SignUp } from "./pages";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 import Signup from "./pages/signup";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
-  const user = null;
+  const { user } = useAuthListener();
+  // console.log(user);
 
   return (
     <Router>
@@ -15,7 +17,6 @@ export default function App() {
           user={user}
           loggedInPath={ROUTES.BROWSE}
           path={ROUTES.SIGN_IN}
-          exact
         >
           <Signin />
         </IsUserRedirect>
@@ -23,7 +24,6 @@ export default function App() {
           user={user}
           loggedInPath={ROUTES.BROWSE}
           path={ROUTES.SIGN_UP}
-          exact
         >
           <Signup />
         </IsUserRedirect>
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useContent } from "../hooks";
 
 export default function Browse() {
-  return <p>Hello from the Browse</p>;
+  // we need the seris and the films
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+
+  // we need slides
+  // pass it to the browse container
+
+  return <p>Hello from the browse!</p>;
 }
